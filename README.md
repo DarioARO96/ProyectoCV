cd23a4b8a1c9b834e27d4cbb88fca23c9054b64b
unknown <roblesollivier@gmail.com>
2023-02-28 20:57:18 -0300

    subiendo

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProyectoFinalCV
+# ProyectoFinalCV
---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("header");
+
+button.addEventListener("click", function () {
+  button.style.backgroundColor = "darkslategrey";
+  button.style.color = "white";
+});
